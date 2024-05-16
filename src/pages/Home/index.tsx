9a1dee4a6d45d1000dff0b053373c6a001ec0c0a
casagrande <casagrandesoaresj@gmail.com>
2024-05-15 23:15:55 -0300

    aprimorando os inputs - done

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,13 +7,23 @@ export function Home () {
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput id="task" placeholder="qual tarefa? ✅"/>
+                    <TaskInput id="task" list="task-suggestions" placeholder="qual tarefa? ✅"/>
+
+                    <datalist id="task-suggestions">
+                        <option value="arrumar casa"></option>
+                        <option value="trabalhar"></option>
+                        <option value="se alongar"></option>
+                    </datalist>
                     
                     <label htmlFor="">durante</label>
                     <MinutesAmountInput 
                         type="number" 
                         id="minutesAmount" 
                         placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+
                     />
 
                     <span>minutos.</span>
