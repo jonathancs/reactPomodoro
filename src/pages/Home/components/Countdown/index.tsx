b5cc6c2c1c36320d88bd0d14372eb3bd764bd45b
Casagrande <casagrandesoaresj@gmail.com>
2024-06-20 23:00:08 -0300

    5-2 converting to context

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,6 +1,69 @@
+import { useContext, useEffect, useState } from "react";
 import { CountdownContainer, Separator } from "./styles";
+import { CyclesContext } from "../..";
+import { differenceInSeconds } from "date-fns";
 
 export function Countdown() {
+  const {activeCycle, activeCycleId, markCurrentCycleAsFinished} = useContext(CyclesContext)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+  const minutes = String(minutesAmount).padStart(2, "0");
+  const seconds = String(secondsAmount).padStart(2, "0");
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
+
+
+
+  useEffect(() => {
+    /* 
+      // useEffect, monitors a variable, whenever it changes, and executes a function.
+      // 1st parameter: which function will be executed, 2nd parameter: which variable to monitor
+      // useEffect(() = {}, [])
+      // useEffect(() = {FUNCTION_TO_BE_EXECUTED}, [VARIABLE_TO_MONITOR])
+
+      // Remember that it executes its functions THE 1ST TIME the element is rendered.
+
+      // Leaving the 2nd parameter empty [], it will only execute when rendered.
+      // you can have code on a react component that only executes once.
+      // usefull for api and database calls.
+    */
+
+    let interval: number;
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate
+        );
+
+        if (secondsDifference >= totalSeconds) {
+          markCurrentCycleAsFinished()
+          setAmountSecondsPassed(totalSeconds)
+          clearInterval(interval)
+        } else {
+          setAmountSecondsPassed(secondsDifference);
+        }
+      }, 1000);
+    }
+
+    // useEffect can receive a return
+    // this return will be triggered WHEN the NEXT re-render of the useEffect happen
+    // useful to clear any information of the previous rendered useEffect
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeCycle, activeCycleId, totalSeconds])
+
     return (
         <CountdownContainer>
           <span>{minutes[0]}</span>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,30 +1,14 @@
 import { HandPalm, Play } from "phosphor-react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import * as zod from "zod";
-import { useEffect, useState } from "react";
-import { differenceInSeconds } from "date-fns";
+import { createContext, useEffect, useState } from "react";
 
 import {
-  CountdownContainer,
-  FormContainer,
   HomeContainer,
-  MinutesAmountInput,
-  Separator,
   StartCountdownButton,
   StopCountdownButton,
-  TaskInput,
 } from "./styles";
 import { Countdown } from "./components/Countdown";
 import { NewCycleForm } from "./components/newCycleForm";
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, "Informe a tarefa"),
-  minutesAmount: zod
-    .number()
-    .min(1, "O ciclo precisa ser de no mínimo 5 minutos.")
-    .max(60, "O ciclo precisa ser de no máximo 60 minutos."),
-});
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
 interface Cycle {
   id: string;
   task: string;
@@ -34,70 +18,19 @@ interface Cycle {
   finishedDate?: Date;
 }
 
+interface CyclesContextType {
+  activeCycle: Cycle | undefined
+  activeCycleId: string | null
+  markCurrentCycleAsFinished: () => void
+}
+
+export const CyclesContext = createContext({} as CyclesContextType)
+
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: "",
-      minutesAmount: 0,
-    },
-  });
-
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
 
-  useEffect(() => {
-    /* 
-      // useEffect, monitors a variable, whenever it changes, and executes a function.
-      // 1st parameter: which function will be executed, 2nd parameter: which variable to monitor
-      // useEffect(() = {}, [])
-      // useEffect(() = {FUNCTION_TO_BE_EXECUTED}, [VARIABLE_TO_MONITOR])
-
-      // Remember that it executes its functions THE 1ST TIME the element is rendered.
-
-      // Leaving the 2nd parameter empty [], it will only execute when rendered.
-      // you can have code on a react component that only executes once.
-      // usefull for api and database calls.
-    */
-
-    let interval: number;
-
-    if (activeCycle) {
-      interval = setInterval(() => {
-        const secondsDifference = differenceInSeconds(
-          new Date(),
-          activeCycle.startDate
-        );
-
-        if (secondsDifference >= totalSeconds) {
-          setCycles((state) =>
-            state.map((cycle) => {
-              if (cycle.id == activeCycleId) {
-                return { ...cycle, finishedDate: new Date() };
-              } else {
-                return cycle;
-              }
-            })
-          )
-          
-          setAmountSecondsPassed(totalSeconds)
-          clearInterval(interval)
-        } else {
-          setAmountSecondsPassed(secondsDifference);
-        }
-      }, 1000);
-    }
-
-    // useEffect can receive a return
-    // this return will be triggered WHEN the NEXT re-render of the useEffect happen
-    // useful to clear any information of the previous rendered useEffect
-    return () => {
-      clearInterval(interval);
-    };
-  }, [activeCycle, activeCycleId, totalSeconds]);
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime());
@@ -113,6 +46,18 @@ export function Home() {
     setActiveCycleId(id);
     setAmountSecondsPassed(0);
     reset();
+  }
+
+  function markCurrentCycleAsFinished() {
+    setCycles((state) =>
+      state.map((cycle) => {
+        if (cycle.id == activeCycleId) {
+          return { ...cycle, finishedDate: new Date() };
+        } else {
+          return cycle;
+        }
+      }),
+    )
   }
 
   function handleInterruptCycle() {
@@ -134,18 +79,9 @@ export function Home() {
     setActiveCycleId(null);
   }
 
-  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+  
 
-  const minutesAmount = Math.floor(currentSeconds / 60);
-  const secondsAmount = currentSeconds % 60;
-  const minutes = String(minutesAmount).padStart(2, "0");
-  const seconds = String(secondsAmount).padStart(2, "0");
-
-  useEffect(() => {
-    if (activeCycle) {
-      document.title = `${minutes}:${seconds}`;
-    }
-  }, [minutes, seconds, activeCycle]);
+  
 
   const task = watch("task");
   const isSubmitDisable = !task;
@@ -155,8 +91,10 @@ export function Home() {
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)}>
+      <CyclesContext.Provider value={{activeCycle, activeCycleId, markCurrentCycleAsFinished}}>
         <NewCycleForm/>
         <Countdown/>
+      </CyclesContext.Provider>
 
         {activeCycle ? (
           <StopCountdownButton onClick={handleInterruptCycle} type="button">
