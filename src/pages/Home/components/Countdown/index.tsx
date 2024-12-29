e21b60df34eadd9f774e6b8b00dffe8f8d0cc09c
Casagrande <casagrandesoaresj@gmail.com>
2024-12-29 02:40:05 -0300

    5-2

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,8 +1,51 @@
-import { CountdownContainer, Separator } from './styles'
-
-
+import { differenceInSeconds } from "date-fns";
+import { useContext, useEffect, useState } from "react";
+import { CountdownContainer, Separator } from "./styles";
+import { CyclesContext } from "../..";
 
 export function Countdown() {
+  const { activeCycle, activeCycleId, markCurrentCycleAsFinished } = useContext(CyclesContext);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+
+  useEffect(() => {
+    let interval: number;
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate
+        );
+
+        if (secondsDifference >= totalSeconds) {
+          markCurrentCycleAsFinished()  
+          setAmountSecondsPassed(totalSeconds);
+          clearInterval(interval);
+        } else {
+          setAmountSecondsPassed(secondsDifference);
+        }
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeCycle, totalSeconds, activeCycleId]);
+
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+  const minutes = String(minutesAmount).padStart(2, "0");
+  const seconds = String(secondsAmount).padStart(2, "0");
+  
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
+
   return (
     <CountdownContainer>
       <span>{minutes[0]}</span>
@@ -11,5 +54,5 @@ export function Countdown() {
       <span>{seconds[0]}</span>
       <span>{seconds[1]}</span>
     </CountdownContainer>
-  )
+  );
 }
